1b4b9555e1fd790eb7671fb666a4d5f04c7cb59a
wima1605 <wima1605@colorado.edu>
2023-04-12 12:57:07 -0600

    Index.js and app.listen(3000)

--- All Project Code and Components/components/src/index.js
+++ All Project Code and Components/components/src/index.js
@@ -118,7 +118,9 @@ app.post('/login', async (req, res) => {
   }
 });
 
-
+app.get('/welcome', (req, res) => {
+  res.json({status: 'success', message: 'Welcome!'});
+});
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
